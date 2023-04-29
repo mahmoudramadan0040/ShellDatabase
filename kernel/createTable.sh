659df6ea7e2b40f2268a8703e6baf7574b3db18b
Mahmoud Ramadan <95087747+mahmoudramadan0040@users.noreply.github.com>
2023-04-29 09:00:49 +0200

    Update createTable.sh

--- kernel/createTable.sh
+++ kernel/createTable.sh
@@ -7,15 +7,15 @@ create_table() {
     read tablename
 
     # Check if the table file already exists
-    if [[ -f "${DB_ROOT_DIR}/${DB_CURRENT}/${tablename}" ]]; then
+    if [[ -f "./databases/${DB_CURRENT}/${tablename}" ]]; then
         echo "Table already exists!"
         return
     fi
 
-    echo "Enter column names separated by ':'"
+    echo "Enter column names separated by ':' eg.id:age"
     read columns
 
-    echo "Enter column datatypes separated by ':' (int or float or string):"
+    echo "Enter column datatypes separated by ':' (int or float or string): eg. int:string"
     read datatypes
 
   # Check if the number of columns and datatypes match
@@ -23,6 +23,7 @@ create_table() {
         echo "Error: number of columns and datatypes do not match"
         return 1
     fi
+    echo $columns >> "./databases/${DB_CURRENT}/${tablename}"
 
     # Validate column data types
     for data_type in $(echo $datatypes | tr ':' ' '); do
@@ -42,7 +43,8 @@ create_table() {
     fi
 
     # Write meta data to file
-    meta_file="${DB_ROOT_DIR}/${DB_CURRENT}/.${tablename}"
+    meta_file="./databases/${DB_CURRENT}/.${tablename}"
+    $columns >> "./databases/${DB_CURRENT}/${tablename}"
     for i in $(echo $columns | tr ':' ' '); do
         col_name=$i
         if [ "$i" == "$primarykey" ]; then
@@ -54,12 +56,11 @@ create_table() {
         datatypes=${datatypes#*:}
     done
 
-    touch "${DB_ROOT_DIR}/${DB_CURRENT}/${tablename}"
+    touch "./databases/${DB_CURRENT}/${tablename}"
     echo "Table created successfully."
 }
 
 create_table
-
 
 # back to Table menu
 source ./database.sh
